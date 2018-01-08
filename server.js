a66308bbd64cf06267642a3ccca2ab07d32c0f87
ninjadev1030 <dalianwang1030@yahoo.com>
2018-01-09 00:58:41 +0800

    update project

--- server.js
+++ server.js
@@ -1,46 +1,35 @@
-// Get dependencies
-const express = require('express');
 const path = require('path');
-const http = require('http');
-const bodyParser = require('body-parser');
-// Get our API routes
-const api = require('./server/routes/api');
+const express = require('express');
 const app = express();
-// Parsers for POST data
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-// Point static path to dist
-app.use(express.static(path.join(__dirname, 'dist')));
-// Set our api routes
-app.use('/api', api);
-// If an incoming requrest uses a protocal other than HTTPS, redirect that request to the same url but with HTTPS
+
+// If an incoming request uses
+// a protocol other than HTTPS,
+// redirect that request to the
+// same url but with HTTPS
 const forceSSL = function() {
-  return function(req, res, next) {
+  return function (req, res, next) {
     if (req.headers['x-forwarded-proto'] !== 'https') {
       return res.redirect(['https://', req.get('Host'), req.url].join(''));
     }
     next();
   }
 }
-// Instruct the app to use the forceSSl middleware
+
+// Instruct the app
+// to use the forceSSL
+// middleware
 app.use(forceSSL());
-// Catch all other routes and return the index file
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
+
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
-/**
- * Get port from environment and store in Express.
- */
-const port = process.env.PORT || '3000';
-app.set('port', port);
-
-/**
- * Create HTTP server.
- */
-const server = http.createServer(app);
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-server.listen(port, () => console.log(`API running on localhost:${port}`));
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
